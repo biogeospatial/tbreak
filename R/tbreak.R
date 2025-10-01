3ec165d01d6effe35d6e269ee57bb75e7d5011e6
Shawn Laffan <shawnlaffan@gmail.com>
2025-10-01 15:27:34 +1000

    more tweaks
    - Plot a basic time series without running anything else.
    - Cut nodata from several analyses (-0.3 for modis)
    - bfast h-parameter as an arg

--- R/tbreak.R
+++ R/tbreak.R
@@ -9,13 +9,13 @@ library("terra")
 library("ncdf4")
 
 beast_modis = function (raster, start_time = NULL, ...) {
-  
+
   #  work on a copy in case it is not in memory
   #  then the copy should be GC'd when that is called
   tmp_ras = raster
   tmp_ras = toMemory(tmp_ras)   # To use beast, make sure all the data is read into memory
   dims    = dim(tmp_ras)
-  
+
   #  generate time axis if needed
   #  assumes form 2024-01-24 somewhere in band name
   if (all (is.na(time(tmp_ras)))) {
@@ -25,13 +25,13 @@ beast_modis = function (raster, start_time = NULL, ...) {
     dates = regmatches(dates, m)
     terra::time(tmp_ras) = strptime(dates, "%Y-%m-%d")
   }
-  
+
   # Y = values(tmp_ras)
   #dim(Y)   = dims[c(2,1,3)]    # Assign column-major dim expected by Rbeast
   #  avoid the need to transpose
   Y = as.matrix(tmp_ras, wide=TRUE)
   dim(Y)   = dims[c(1,2,3)]  #  could just use dims directly...
-  
+
   metadata = list(
     time             = time(tmp_ras),
     isRegularOrdered = FALSE,    # IRREGULAR input
@@ -45,36 +45,36 @@ beast_modis = function (raster, start_time = NULL, ...) {
     #period           = 16/365
     #startTime        = ifelse (is.null(start_time, time(tmp_ras)[1], as.Date(start_time)))
   )
-  
+
   #  minimal for now
   extra = list (
     numThreadsPerCPU = 3,
     numParThreads    = 30
   )
-  
+
   #browser()
   tmp_ras = NULL
   gc()
-  
-  
+
+
   o = Rbeast::beast123 (Y, metadata = metadata, extra = extra, ...)
-  
+
   #  pack in some extra info - underhanded but works for now
   o$ext = as.vector(ext(raster)) #  ext objects do not survive serialisation otherwise
   o$crs = crs(raster)
-  
+
   gc()
   return (o)
 }
 
 parse_arcgis_coord = function (coord) {
   coord = stri_trim(coord)
-  
+
   in_metres = stri_sub(coord, -1) == "m"
-  
+
   xy = stri_split_regex(coord, "\\s")[[1]][1:2]
   xy = stri_replace_all_fixed (xy, replacement="", pattern=",")
-  
+
   if (!in_metres) {
     library("parzer")
     library("sf")
@@ -87,21 +87,21 @@ parse_arcgis_coord = function (coord) {
     p = st_coordinates(point2)
     return (c(p[1], p[2]))
   }
-  
+
   x_hemi = stri_extract(xy[1], regex = "[NESW]")
   x = stri_replace_all_fixed(xy[1], replacement = "", pattern = x_hemi)
   x = as.numeric(x)
   if (x_hemi == "W") {
     x = x * -1
   }
-  
+
   y_hemi = stri_extract(xy[2], regex = "[NESW]")
   y = stri_replace_all_fixed(xy[2], replacement = "", pattern = y_hemi)
   y = as.numeric(y)
   if (y_hemi == "S") {
     y = y * -1
   }
-  
+
   return (c(x, y))
 }
 
@@ -121,22 +121,22 @@ coord2idx_rbeast = function (b, coord) {
   coord = parse_arcgis_coord(coord)
   x = coord[1]
   y = coord[2]
-  
+
   extent = ext(b$ext)
-  
+
   if (x < extent$xmin || x >= extent$xmax) {
     stop ("X coord is outside data set extent")
   }
   if (y < extent$ymin || y >= extent$ymax) {
     stop ("Y coord is outside data set extent")
   }
-  
+
   ncol = b$ncols
   nrow = b$nrows
-  
+
   c = 1    + floor ((x - extent$xmin) / (extent$xmax - extent$xmin) * ncol)
   r = nrow - floor ((y - extent$ymin) / (extent$ymax - extent$ymin) * nrow)
-  
+
   res = c(r, c, use.names=FALSE)
   return (res)
 }
@@ -144,8 +144,8 @@ coord2idx_rbeast = function (b, coord) {
 
 plot_beast_modis_coord = function (b, coord, t=FALSE) {
   rowcol = coord2idx_rbeast(b, coord)
-  
-  
+
+
   #  transpose for beast coords
   if (t) {
     rc = rowcol[c(2,1)]
@@ -157,7 +157,7 @@ plot_beast_modis_coord = function (b, coord, t=FALSE) {
   message ("Mean number of trend change points is ",  b$trend$ncp[rc[1],rc[2]])
   message ("Mean number of season change points is ", b$season$ncp[rc[1],rc[2]])
   plot (b[rc])
-  
+
 }
 
 load_data = function (file = NULL, drivers=NULL) {
@@ -186,12 +186,12 @@ rasterise_beast = function (b) {
   if (is.null(b$ext)) {
     stop ("An extent must be set on the beast object")
   }
-  
+
   b$time_as_date = lubridate::date_decimal (b$time)
-  
+
   results = list ()
   trend_ncp = beastbit2raster (b, "trend", "ncp")
-  
+
   for (component in c("trend", "season")) {
     for (subcomponent in names(b[[component]])) {
       if (!is.null (b[[component]][[subcomponent]])) {
@@ -206,8 +206,8 @@ rasterise_beast = function (b) {
     label = component
     results[[label]] = beastbit2raster(b, component)
   }
-  
-  
+
+
   results
 }
 
@@ -215,14 +215,14 @@ beastbit2raster = function (b, component = "trend", subcomponent = "ncp", templa
   if (class(b) != "beast") {
     stop ("Need an Rbeast result")
   }
-  
+
   temporal_subcomponents = c(
-    "cpOccPr", "Y", "SD", "order", 
+    "cpOccPr", "Y", "SD", "order",
     "slp",     "slpSD",
     "slpSgnPosPr", "slpSgnZeroPr",
     "amp",     "ampSD"
   )
-  
+
   valid_components = c("trend", "season", "R2", "RMSE", "sig2", "marg_lik")
   if (!(component %in% valid_components)) {
     stop (cat ("component args must be in list: ", paste(valid_components, sep=", ")))
@@ -230,18 +230,18 @@ beastbit2raster = function (b, component = "trend", subcomponent = "ncp", templa
   if ((component %in% c("trend", "season")) && is.null(b[[component]][[subcomponent]])) {
     stop (sprintf ("Cannot find b$%s$%s, or it is null", component, subcomponent))
   }
-  
+
   ext = b$ext
   if (is.null(ext) && (is.null (template) || !hasMethod("ext", class(template)))) {
     stop ("No extent found. Need an Rbeast object with an ext slot, or a template object with an ext method")
   }
   ext = ext(ext) #  ensure we have an extent object
-  
+
   crs = b$crs
   if (is.null(crs) && (is.null (template) || !hasMethod("crs", class(template)))) {
     stop ("No coord sys found.  Need an Rbeast object with a crs slot, or a template object with a crs method")
   }
-  
+
   rasterise = function (data) {
     m = matrix(
       data,
@@ -251,9 +251,9 @@ beastbit2raster = function (b, component = "trend", subcomponent = "ncp", templa
     )
     r = terra::rast(m, extent=ext, crs=crs)
   }
-  
+
   r = NULL
-  
+
   if (component %in% c("trend", "season")) {
     subdata = b[[component]][[subcomponent]]
     dims = dim(subdata)
@@ -269,7 +269,7 @@ beastbit2raster = function (b, component = "trend", subcomponent = "ncp", templa
       r = do.call("c", lapply(1:nbands, function(i){
         return (rasterise (subdata[,,i]))
       }))
-      
+
       #  These should be temporal data.
       if (subcomponent %in% temporal_subcomponents && nbands == length(b$time)) {
         message (sprintf ("Temporal data for %s, %s", component, subcomponent))
@@ -279,23 +279,23 @@ beastbit2raster = function (b, component = "trend", subcomponent = "ncp", templa
         }
         terra::time(r) = t
       }
-      
+
       names(r) = paste0 (
         sprintf ("%s_%s", component, subcomponent),
         formatC(1:nbands, flag="0", width=nchar(nbands))
       )
-      
+
     }
     else {
       ci_names = c("lower", "upper")
       r = list()
       for (j in 1:2) {
-        
+
         nbands = dims[3]
         rr = do.call("c", lapply(1:nbands, function(i){
           return (rasterise (subdata[,,i,j]))
         }))
-        
+
         names(rr) = paste0 (
           sprintf ("%s_%s_%s", component, subcomponent, ci_names[j]),
           formatC(1:nbands, flag="0", width=nchar(nbands))
@@ -307,7 +307,7 @@ beastbit2raster = function (b, component = "trend", subcomponent = "ncp", templa
           r = rr
         }
       }
-      
+
     }
   }
   else {
@@ -316,7 +316,7 @@ beastbit2raster = function (b, component = "trend", subcomponent = "ncp", templa
     r = rasterise (subdata)
     names(r) = component
   }
-  
+
   r
 }
 
@@ -342,7 +342,7 @@ export_beast_rasters = function (b, dir, prefix="", overwrite=FALSE) {
 
 
 
-plot_bfast_modis_coord = function (raster, coord) {
+plot_bfast_modis_coord = function (raster, coord, h=0.15) {
   coord = parse_arcgis_coord(coord)
 
   #  generate time axis if needed
@@ -360,13 +360,15 @@ plot_bfast_modis_coord = function (raster, coord) {
     #  nasty but we otherwise get bfastts errors
     dates = strptime(strftime(dates, format="%Y%m%d"), "%Y%m%d")
   }
-  
+
   cell_num = terra::cellFromXY (raster, cbind (x = coord[1], y = coord[2]))
   if (is.na(cell_num)) {
     stop ("Coord does not intersect the raster")
   }
-  t2 = bfast::bfastts(unlist(raster[cell_num]), dates, type = '16-day')
-  tb = bfast::bfast(t2)
+  u = unlist(raster[cell_num])
+  u[u < -0.25] = NA
+  t2 = bfast::bfastts(u, dates, type = '16-day')
+  tb = bfast::bfast(t2, h=h)
   plot(tb)
   invisible (tb)
 }
@@ -391,7 +393,7 @@ assign_time_to_raster = function (raster, format= "%Y-%m-%d") {
 
 plot_ts_modis_coord = function (raster, coord) {
     coord = parse_arcgis_coord(coord)
-    
+
     #  generate time axis if needed
     #  assumes form 2024-01-24 somewhere in band name
     #  dup from above - should be a function
@@ -407,16 +409,25 @@ plot_ts_modis_coord = function (raster, coord) {
       #  nasty but we otherwise get bfastts errors
       dates = strptime(strftime(dates, format="%Y%m%d"), "%Y%m%d")
     }
-    
+
     cell_num = terra::cellFromXY (raster, cbind (x = coord[1], y = coord[2]))
     if (is.na(cell_num)) {
       stop ("Coord does not intersect the raster")
     }
     u = unlist(raster[cell_num])
-    z = zoo(u, dates)
+    u[u < -0.25] = NA  #  -0.3 is nodata
+    z = zoo::zoo(u, dates)
     plot (z, xlab = "Index", ylab = "Date")
+
+    #  do we have any NAs?  highlight vals before and after
+    na_prev_z = is.na(c(FALSE, z[-length(z)]))
+    points (z[na_prev_z], col="red", pch = 16)
+    na_next_z = c(is.na(z[-1]), FALSE)
+    points (z[na_next_z], col="blue", pch=16)
+
     if (any(is.na(u))) {
       warning("NAs found in time series - break analyses might not work")
     }
     invisible(z)
 }
+
